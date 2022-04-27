31dbc8b0a682bfa234c153588a3f1a81690a7d8a
Spi051997 <dcodeacedemy97@gmail.com>
2022-04-27 05:32:10 +0530

    Added usestate method

--- src/Hooks.js
+++ src/Hooks.js
@@ -2,19 +2,29 @@ import React,{useState} from "react";
 import "./App.css";
 
 function Hooks() {
-     const emailstate=useState('a@gmailcom')
-    const email=emailstate[0];
-    const setEmail=emailstate[1];
+     const [email,setEmail]=useState('a@gmailcom')
+    // const email=emailstate[0];
+    // const setEmail=emailstate[1];
+
+
+    const [name,setname]=useState('')
+    // const name=namestate[0];
+    // const setname=namestate[1];
     function handleoncange(e)
     {
         setEmail(e.target.value)
-
-
+    }
+    function handlename(e)
+    {
+        setname(e.target.value)
     }
   return (
     <div className="App">
       <input value={email} onChange={handleoncange} />
+      <br></br>
+      <input value={name} onChange={handlename} />
       <p>{email}</p>
+      <p>{name}</p>
     </div>
   );
 }
